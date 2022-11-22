a81fe06f2723d5c78642a67e6d0068c679af5a47
Saqib <iamsaqib24@gmail.com>
2022-11-22 15:33:17 +0530

    javascript-dom

---
+++ index.js
@@ -0,0 +1,34 @@
+// DOM Manipulation
+
+// getElementById()
+
+const title = document.getElementById('main-heading');
+
+console.log(title);
+
+// getElementByClassName()
+
+// const listItem = document.getElementsByClassName('list-item');
+
+// console.log(listItem);
+
+// getElementByTagName()
+
+const listItem = document.getElementsByTagName('li');
+
+console.log(listItem);
+
+
+
+// querySelector()
+
+// const container = document.querySelector('div');
+
+// console.log(container);
+
+
+// querySelectorAll()
+
+const container = document.querySelectorAll('div');
+
+console.log(container);
